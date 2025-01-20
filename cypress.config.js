0f83d6ce3975356ef49484e2ff045cbf1bec297f
brunogsiq <brunogsiq@gmail.com>
2025-01-20 11:12:34 -0300

    New files

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig(
+{
+    viewportWidth: 1300,
+    viewportHeight: 800,
+    video: true,
+    chromeWebSecurity: false,
+    reporter: "mochawesome",
+    reporterOptions: {
+        reportDir: 'cypress/report',
+        overwrite: true,
+        html: true,
+        json: false,
+        timestamp: 'dd-mm-yyyy_HH-MM-ss'
+    },
+    e2e: {
+        defaultCommandTimeout: 9000,
+        experimentalRunAllSpecs: true,
+        hideXHRInCommandLog: true,
+        baseUrl: "https://starbugs.vercel.app/",
+        //Aqui ficarão todos os meus testes:
+        setupNodeEvents(on, config) {
+          // implement node event listeners here
+        },
+    },
+});
